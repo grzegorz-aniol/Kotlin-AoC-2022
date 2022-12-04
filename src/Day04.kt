502ddf80701802dbdedc281fb2c0219c518996f0
Grzegorz Aniol <grzegorz.aniol@graphaware.com>
2022-12-04 09:38:53 +0100

    Solutions for Day 4

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,10 +1,5 @@
 fun main() {
 
-    fun fullyContains(r1: IntRange, r2: IntRange) =
-        r1.intersect(r2).let { it ->
-            it.size == r1.count() || it.size == r2.count()
-        }
-
     fun List<String>.splitToRanges(): List<Pair<IntRange, IntRange>> =
         map { text ->
             val numbers = text.split(",").flatMap { it.split("-") }.map { it.toInt() }
@@ -12,18 +7,26 @@ fun main() {
             Pair(numbers[0]..numbers[1], numbers[2]..numbers[3])
         }
 
-    fun part1(input: List<String>): Int =
-        input.splitToRanges().count {fullyContains(it.first, it.second) }
+    fun IntRange.contains(other: IntRange) =
+        other.first in this && other.last in this
 
-    fun part2(input: List<String>): Int {
-        return 0
-    }
+    fun fullyContains(ranges: Pair<IntRange, IntRange>) =
+        ranges.first.contains(ranges.second) || ranges.second.contains(ranges.first)
+
+    fun overlaps(pairOfRanges: Pair<IntRange, IntRange>) =
+        pairOfRanges.first.intersect(pairOfRanges.second).isNotEmpty()
+
+    fun part1(input: List<String>): Int =
+        input.splitToRanges().count(::fullyContains)
+
+    fun part2(input: List<String>): Int =
+        input.splitToRanges().count(::overlaps)
 
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 2) { "Part1 Test - expected value doesn't match" }
-//    check(part2(testInput) == 70) { "Part2 Test - expected value doesn't match" }
+    check(part2(testInput) == 4) { "Part2 Test - expected value doesn't match" }
 
     val input = readInput("Day04")
-    println(part1(input)) //
-//    println(part2(input)) //
+    println(part1(input))
+    println(part2(input))
 }
