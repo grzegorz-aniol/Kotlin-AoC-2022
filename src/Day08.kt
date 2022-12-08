426161555548f0f07d0b90e0ba0f7f31ed63a824
Grzegorz Aniol <grzegorz.aniol@graphaware.com>
2022-12-08 19:56:14 +0100

    Refactored solution for day 8

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,89 +1,70 @@
+typealias Matrix = Array<IntArray>
+typealias BreakFun = (score: Long, startHeight: Int, height: Int) -> Pair<Boolean, Long>
+typealias TermFun = (score: Long) -> Long
+typealias AccFun = (Long, Long, Long, Long, Long) -> Long
+
 fun main() {
 
-    fun parseMap(input: List<String>): Array<IntArray> {
-        val array = Array(input.size) { IntArray(input.first().length) }
-        for ((row, line) in input.withIndex()) {
-            for ((col, ch) in line.withIndex()) {
-                array[row][col] = Integer.parseInt(ch.toString())
+    fun List<String>.toMatrix() = map { it.map { it - '0' }.toIntArray() }.toTypedArray()
+
+    fun Matrix.walk(sx: Int, sy: Int, dx: Int, dy: Int, breakFun: BreakFun, termFun: TermFun): Long {
+        var (x, y) = sx to sy
+        val (rows, cols) = this.size to this.first().size
+        var points = 0L
+        while (x in 0 until cols && y in 0 until rows) {
+            if (x != sx || y != sy) {
+                points += 1
+                val breakStatus = breakFun(points, this[sy][sx], this[y][x])
+                if (breakStatus.first) {
+                    return breakStatus.second
+                }
             }
+            x += dx
+            y += dy
         }
-        return array
+        return termFun(points)
     }
 
-    fun part1(input: List<String>): Int {
-        val map = parseMap(input)
-        val rows = input.size
-        val cols = input.first().length
-        val visibilityMap = Array(rows) { IntArray(cols) { 0 } }
-
-        fun analyzeLine(sx: Int, sy: Int, dx: Int, dy: Int) {
-            var height = -1
-            var x = sx
-            var y = sy
-            while (x in 0 until cols && y in 0 until rows) {
-                if (map[y][x] > height) {
-                    visibilityMap[y][x] = 1
-                }
-                height = Math.max(height, map[y][x])
-                x += dx
-                y += dy
+    fun Matrix.calculateScore(breakFun: BreakFun, termFun: TermFun, accFun: AccFun): Long {
+        val (rows, cols) = this.size to this.first().size
+        var score = 0L
+        for (y in 0 until rows) {
+            for (x in 0 until cols) {
+                val v1 = walk(x, y, 1, 0, breakFun, termFun)
+                val v2 = walk(x, y, -1, 0, breakFun, termFun)
+                val v3 = walk(x, y, 0, 1, breakFun, termFun)
+                val v4 = walk(x, y, 0, -1, breakFun, termFun)
+                score = accFun(score, v1, v2, v3, v4)
             }
         }
+        return score
+    }
 
-        for (inrows in 0 until rows) {
-            analyzeLine(0, inrows, 1, 0)
-            analyzeLine(cols - 1, inrows, -1, 0)
-        }
-        for (incols in 0 until cols) {
-            analyzeLine(incols, 0, 0, 1)
-            analyzeLine(incols, rows-1, 0, -1)
-        }
-        return visibilityMap.sumOf { it.sum() }
+    fun part1(input: List<String>): Long {
+        val matrix = input.toMatrix()
+
+        val breakFun: BreakFun = { _, startHeight: Int, height: Int -> (height >= startHeight) to 0 }
+        val termFun: TermFun = { _ -> 1 }
+        val accFun: AccFun = { prev, v1, v2, v3, v4 -> prev + Math.min(1, v1 + v2 + v3 + v4) }
+
+        return matrix.calculateScore(breakFun, termFun, accFun)
     }
 
     fun part2(input: List<String>): Long {
-        val map = parseMap(input)
-        val rows = input.size
-        val cols = input.first().length
+        val matrix = input.toMatrix()
 
-        fun scoreView(sx: Int, sy: Int, dx: Int, dy: Int): Long {
-            val pointHeight = map[sy][sx]
-            var x = sx
-            var y = sy
-            var score = 0L
-            while (x in 0 until cols && y in 0 until rows) {
-                if (x != sx || y != sy) {
-                    score += 1
-                    val height = map[y][x]
-                    if (height >= pointHeight) {
-                        break
-                    }
-                }
-                x += dx
-                y += dy
-            }
-            return score
-        }
+        val breakFun: BreakFun = { score, startHeight: Int, height: Int -> (height >= startHeight) to score }
+        val termFun: TermFun = { score -> score }
+        val accFun: AccFun = { prev, v1, v2, v3, v4 -> Math.max(prev, v1 * v2 * v3 * v4) }
 
-        var maxScore = 0L
-        for (y in 0 until rows - 1) {
-            for (x in 0 until cols - 1) {
-                val s1 = scoreView(x, y, 1, 0)
-                val s2 = scoreView(x, y, -1, 0)
-                val s3 = scoreView(x, y, 0, 1)
-                val s4 = scoreView(x, y, 0, -1)
-                val totalScore = s1 * s2 * s3 * s4
-                maxScore = Math.max(maxScore, totalScore)
-            }
-        }
-        return maxScore
+        return matrix.calculateScore(breakFun, termFun, accFun)
     }
 
     val testInput = readInput("Day08_test")
-    check(part1(testInput).also { println(it) } == 21) { "Part1 Test - expected value doesn't match" }
+    check(part1(testInput).also { println(it) } == 21L) { "Part1 Test - expected value doesn't match" }
     check(part2(testInput).also { println(it) } == 8L) { "Part2 Test - expected value doesn't match" }
 
     val input = readInput("Day08")
-    println(part1(input)) // 1684
-    println(part2(input)) // 486540
+    println(part1(input))
+    println(part2(input))
 }
