78523d5e5063caa8227aa0f861da33f344272917
Grzegorz Aniol <grzegorz.aniol@graphaware.com>
2022-12-08 08:46:48 +0100

    Solution for day 8 part 2

--- src/Day08.kt
+++ src/Day08.kt
@@ -41,13 +41,49 @@ fun main() {
         return visibilityMap.sumOf { it.sum() }
     }
 
-    fun part2(input: List<String>) = 0
+    fun part2(input: List<String>): Long {
+        val map = parseMap(input)
+        val rows = input.size
+        val cols = input.first().length
+
+        fun scoreView(sx: Int, sy: Int, dx: Int, dy: Int): Long {
+            val pointHeight = map[sy][sx]
+            var x = sx
+            var y = sy
+            var score = 0L
+            while (x in 0 until cols && y in 0 until rows) {
+                if (x != sx || y != sy) {
+                    score += 1
+                    val height = map[y][x]
+                    if (height >= pointHeight) {
+                        break
+                    }
+                }
+                x += dx
+                y += dy
+            }
+            return score
+        }
+
+        var maxScore = 0L
+        for (y in 0 until rows - 1) {
+            for (x in 0 until cols - 1) {
+                val s1 = scoreView(x, y, 1, 0)
+                val s2 = scoreView(x, y, -1, 0)
+                val s3 = scoreView(x, y, 0, 1)
+                val s4 = scoreView(x, y, 0, -1)
+                val totalScore = s1 * s2 * s3 * s4
+                maxScore = Math.max(maxScore, totalScore)
+            }
+        }
+        return maxScore
+    }
 
     val testInput = readInput("Day08_test")
     check(part1(testInput).also { println(it) } == 21) { "Part1 Test - expected value doesn't match" }
-//    check(part2(testInput) == -1) { "Part2 Test - expected value doesn't match" }
+    check(part2(testInput).also { println(it) } == 8L) { "Part2 Test - expected value doesn't match" }
 
     val input = readInput("Day08")
-    println(part1(input))
-//    println(part2(input))
+    println(part1(input)) // 1684
+    println(part2(input)) // 486540
 }
