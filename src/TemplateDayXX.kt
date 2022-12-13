e2d70c442c7f01cd1560c1714047e63e295bb85f
Grzegorz Aniol <grzegorz.aniol@graphaware.com>
2022-12-13 18:12:47 +0100

    Class template for daily solutions

---
+++ src/TemplateDayXX.kt
@@ -0,0 +1,24 @@
+private const val DAY = 13
+
+class TemplateDayXX {
+
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+}
+
+fun main() {
+    val testInput1 = readInput("Day${DAY}_test")
+    val day = TemplateDayXX()
+    check(day.part1(testInput1).also { println(it) } == -1) { "Part1 Test - expected value doesn't match" }
+    check(day.part2(testInput1).also { println(it) } == -1) { "Part2 Test - expected value doesn't match" }
+
+    val input = readInput("Day${DAY}")
+    println(day.part1(input))
+    println(day.part2(input))
+}
